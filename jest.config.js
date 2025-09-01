6462c547a2dd9ac8bc0c24287578639d0cb130e9
Bobby Bonestell <rbonestell@gmail.com>
2025-08-31 20:06:51 -0600

    Dropped test coverage thresholds in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -48,9 +48,9 @@ export default {
   coverageThreshold: {
     global: {
       branches: 70,
-      functions: 80,
-      lines: 80,
-      statements: 80
+      functions: 70,
+      lines: 70,
+      statements: 70
     }
   },
 
